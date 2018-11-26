f7d8b8d991d863b71353fd8cda2fa7e7d13f6ab4
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2018-11-26 13:31:43 +0100

    Use configured url in all places

--- drink.py
+++ drink.py
@@ -12,23 +12,22 @@ def get_login_token(api_url, password):
     return json_result[u'token']
 
 cfg = config.Config(pathlib.Path("~/.drinklist").expanduser())
-api_url = "https://fius.informatik.uni-stuttgart.de/drinklist/api"
 
 cfg.init_value('url', lambda: "https://fius.informatik.uni-stuttgart.de/drinklist/api")
 cfg.init_value('pw', lambda: getpass.getpass())
-cfg.init_value('token', lambda: get_login_token(api_url, cfg['pw']))
+cfg.init_value('token', lambda: get_login_token(cfg["url"], cfg['pw']))
 cfg.init_value('user', lambda: input("Username: "))
 
 def list_beverages():
     global cfg
-    r = requests.get(api_url + "/beverages", headers={'X-Auth-Token' : cfg['token']})
+    r = requests.get(cfg["url"] + "/beverages", headers={'X-Auth-Token' : cfg['token']})
     j = json.loads(r.text)
     for drink in j:
         print(u"{}\t{}".format(drink["name"], drink["price"]))
 
 def order_drink(drink):
     global cfg
-    r = requests.post(api_url + "/orders",
+    r = requests.post(cfg["url"] + "/orders",
                       headers={'X-Auth-Token': cfg['token']},
                       params={'user': cfg['user'], 'beverage': drink})
     print(r.text)
