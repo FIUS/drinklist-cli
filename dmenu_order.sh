233a9d4ad34b8f664dae459dd9e6860577cdd9fc
Tobias Wältken <tobias.w@ltken.de>
2019-01-25 13:12:13 +0100

    Remove unneccesary white space

--- dmenu_order.sh
+++ dmenu_order.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DRINK=$(drinklist -columns name -format text list | tail -n +3 | dmenu)
+DRINK=$(drinklist -columns name -format text list | tail -n +3 | awk '{$1=$1;print}' | dmenu)
 
 if [ ! -z "$DRINK" ]; then
 	drinklist order $DRINK &> /tmp/drinklist.out && notify-send -u normal "Drinklist $DRINK" "$(cat /tmp/drinklist.out)" || notify-send -u critical "Drinklist $DRINK FAILED" "$(cat /tmp/drinklist.out)"
