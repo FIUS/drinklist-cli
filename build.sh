54e1ab123eaeb7ad8ecea2ec19902053f4dad204
Tobias Wältken <tobias.w@ltken.de>
2019-08-01 14:40:30 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp drink.py __main__.py
-zip drinklist.zip __main__.py config.py ppformat.py
+zip drinklist.zip __main__.py ppformat.py levenshtein.py parameter_store.py utils.py
 echo "#!/usr/bin/env python3" > drinklist
 cat drinklist.zip >> drinklist
 chmod +x drinklist
