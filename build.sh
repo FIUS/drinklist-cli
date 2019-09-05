4fbaeb65c7ad5d95fcbeca104dbcf1592dee2cde
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2019-09-05 18:33:56 +0200

    Change build.sh to new directory structure and update PKGBUILD

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 cp src/drink.py __main__.py
 zip drinklist.zip __main__.py src/ppformat.py src/levenshtein.py src/parameter_store.py src/utils.py
-echo "#!/usr/bin/env python3" > drinklist
-cat drinklist.zip >> drinklist
-chmod +x drinklist
+echo "#!/usr/bin/env python3" > packages/drinklist
+cat drinklist.zip >> packages/drinklist
+chmod +x packages/drinklist
 
 rm __main__.py
 rm drinklist.zip
